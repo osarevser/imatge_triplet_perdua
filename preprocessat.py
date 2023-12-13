01491ddce479c574147520757e94e179c4f428e3
osarevser <osarevser@gmail.com>
2023-12-13 20:35:26 +0100

    Added .gitignore file to ignore all .npz files

--- preprocessat.py
+++ preprocessat.py
@@ -8,13 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-class PreProcessing:
+class PreProcessat:
     def __init__(self,path):
         self.path=path
         print("Loading and processing dataset...")
         self.classes,self.labels_train,self.images_train,self.labels_test,self.images_test=self.preprocessing(0.9)
-        #self.map_train_label_indices={label:np.flatnonzero(self.labels_train==label) for label in self.classes}
-        print('Preprocessing Done. Summary:')
+        self.map_train_label_indices={label:np.flatnonzero(self.labels_train==np.where(self.classes==label)) for label in self.classes}
+        print('Preprocessing done, summary:')
         print("Images train :", self.images_train.shape)
         print("Labels train :", self.labels_train.shape)
         print("Images test  :", self.images_test.shape)
@@ -40,18 +40,15 @@ class PreProcessing:
         labels_shuffled,images_shuffled=labels[shuffle_indices],images[shuffle_indices,:,:]
         train_test=int(train_test_ratio*len(labels))
         return classes,labels_shuffled[:train_test],images_shuffled[:train_test],labels_shuffled[train_test:],images_shuffled[train_test:]
-    
-    def get_triplets(self):
-        label_l,label_r=np.random.choice(self.unique_train_label,2,replace=False)
-        a,p=np.random.choice(self.map_train_label_indices[label_l],2,replace=False)
-        n=np.random.choice(self.map_train_label_indices[label_r])
-        return a,p,n
-    
+        
     def get_triplets_batch(self,n):
-        idxs_a,idxs_p,idxs_n=[],[],[]
+        idxs_a,idxs_p,idxs_n,key=[],[],[],[]
         for _ in range(n):
-            a,p,n=self.get_triplets()
+            label_l,label_r=np.random.choice(self.classes,2,replace=False)
+            a,p=np.random.choice(self.map_train_label_indices[label_l],2,replace=False)
+            n=np.random.choice(self.map_train_label_indices[label_r])
             idxs_a.append(a)
             idxs_p.append(p)
             idxs_n.append(n)
-        return self.images_train[idxs_a,:],self.images_train[idxs_p,:],self.images_train[idxs_n,:]
+            key.append((label_l,label_r))
+        return self.images_train[idxs_a,:],self.images_train[idxs_p,:],self.images_train[idxs_n,:],key
--- train.py
+++ train.py
@@ -5,9 +5,73 @@ Created on Sat Dec  2 22:46:25 2023
 @author: Oscar
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
 import tensorflow as tf
-from preprocessat import PreProcessing
+import keras
+from pathlib import Path
+from keras import applications
+from keras import layers
+from keras import losses
+from keras import ops
+from keras import optimizers
+from keras import metrics
+from keras import Model
+from keras.applications import resnet
+from preprocessat import PreProcessat
+from tripletloss import DistanceLayer,SiameseModel
 
-dataset=PreProcessing("pdb104M_clear.npz")
+image_count=100
+
+split1_dataset=tf.data.Dataset.from_tensor_slices(np.random.rand(image_count,64,64,3))
+split2_dataset=tf.data.Dataset.from_tensor_slices(np.random.rand(image_count,64,64,3))
+split3_dataset=tf.data.Dataset.from_tensor_slices(np.random.rand(image_count,64,64,3))
+
+dataset=tf.data.Dataset.zip((split1_dataset,split2_dataset,split3_dataset))
+
+train_dataset=dataset.take(round(image_count*0.8))
+val_dataset=dataset.skip(round(image_count*0.8))
+
+train_dataset=train_dataset.batch(32,drop_remainder=False)
+val_dataset=val_dataset.batch(32,drop_remainder=False)
+
+target_shape=(64,64)
+
+embedding=keras.Sequential([layers.Input(shape=target_shape+(3,)),
+                            layers.Conv2D(32,(7,7),activation='relu',padding='SAME'),
+                            layers.MaxPooling2D((2,2)),
+                            layers.Conv2D(64,(5,5),activation='relu',padding='SAME'),
+                            layers.MaxPooling2D((2,2)),
+                            layers.Conv2D(128,(3,3),activation='relu',padding='SAME'),
+                            layers.MaxPooling2D((2,2)),
+                            layers.Conv2D(256,(1,1),activation='relu',padding='SAME'),
+                            layers.MaxPooling2D((2,2)),
+                            layers.Conv2D(28,(1,1),activation='relu',padding='SAME'),
+                            layers.MaxPooling2D((2,2)),
+                            layers.Flatten(),
+                            layers.Dense(64)],
+                           name="Embedding")
+
+anchor_input=layers.Input(name="anchor",shape=target_shape+(3,))
+positive_input=layers.Input(name="positive",shape=target_shape+(3,))
+negative_input=layers.Input(name="negative",shape=target_shape+(3,))
+
+distances=DistanceLayer()(embedding(anchor_input),
+                          embedding(positive_input),
+                          embedding(negative_input))
+
+siamese_network=Model(inputs=[anchor_input,positive_input,negative_input],outputs=distances)
+
+siamese_model=SiameseModel(siamese_network)
+siamese_model.compile(optimizer=optimizers.Adam(0.0001))
+history=siamese_model.fit(train_dataset,epochs=25,validation_data=val_dataset)
+
+#%%
+plt.figure()
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.plot(history.history["loss"],label="training loss")
+plt.plot(history.history["val_loss"],label="validation loss")
+plt.legend()
